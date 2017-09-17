2eb4672b7c0bf580cc67e0ffe286bc3cf06a6619
Derek Lewis <derek_lewis@projectivesoftware.com>
2017-09-17 10:22:26 -0500

    Update copyright

--- src/main/java/com/projectivesoftware/EurekaServiceApplication.java
+++ src/main/java/com/projectivesoftware/EurekaServiceApplication.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) Projective Software LLC, 2017 - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
 package com.projectivesoftware;
 
 import org.springframework.boot.SpringApplication;
